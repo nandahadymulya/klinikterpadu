f1b68337e7a7f57ba61ff275e63e07dfec3b2629
nandahadymulya <nandahadymulyaa@gmail.com>
2021-07-13 16:30:23 +0700

    first commit

---
+++ application/views/menu.php
@@ -0,0 +1,48 @@
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="" || $this->uri->segment(1)=="dashboard"){echo "active";}?>" aria-current="page" href="<?php echo site_url("dashboard"); ?>">
+              <span data-feather="home"></span>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="pasien"){echo "active";}?>" href="<?php echo site_url("pasien"); ?>">
+              <span data-feather="file"></span>
+              Pasien
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="petugas"){echo "active";}?>" href="<?php echo site_url("petugas"); ?>">
+              <span data-feather="shopping-cart"></span>
+              Petugas
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="obat"){echo "active";}?>" href="<?php echo site_url("obat"); ?>">
+              <span data-feather="users"></span>
+              Obat
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="aset"){echo "active";}?>" href="<?php echo site_url("aset"); ?>">
+              <span data-feather="bar-chart-2"></span>
+              Aset
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php if($this->uri->segment(1)=="bpjs"){echo "active";}?>" href="<?php echo site_url("bpjs"); ?>">
+              <span data-feather="layers"></span>
+              BPJS
+            </a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      
